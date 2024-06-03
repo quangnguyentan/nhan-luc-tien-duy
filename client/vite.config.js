fc239b599dda587f865c211093780464deaa3ce4
Nguyen Tan Quang <tanquang1510@gmail.com>
2024-06-04 01:17:26 +0700

    commit code

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,59 +1,7 @@
 import { defineConfig } from 'vite'
-import { VitePWA } from 'vite-plugin-pwa'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      devOptions: {
-        enabled: true,
-      },
-      workbox: {
-        skipWaiting: true,
-        clientsClaim: true,
-        globPatterns: ['**/*.{js,css,html,png,jpg,svg,json,webmanifest,ico}'],
-      },
-      includeAssets: ['icons'],
-      strategies: 'injectManifest',
-      srcDir: 'src/sw',
-      filename: 'sw.ts',
-      injectManifest: {
-        swDest: 'dist/sw.js',
-      },
-      manifest: {
-        name: 'WTT - APP',
-        short_name: 'WTT',
-        description: 'An app to track user data',
-        theme_color: '#161618',
-        start_url: '/',
-        display: 'standalone',
-        orientation: 'portrait',
-        icons: [
-          {
-            src: '/icons/android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/icons/android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: "/icons/apple-touch-icon.png",
-            sizes: "180x180",
-            type: "image/png",
-          }
-        ],
-      }
-    })
-  ],
-  base: '/',
-  publicDir: 'public',
-  build: {
-    minify: 'terser',
-  }
+  plugins: [react()],
 })
